a2fe8dc8ee979e87568f2632a8382d8d74540af4
Mmyat <mmyathaha@gmail.com>
2023-12-31 23:17:16 +0630

    dark mode

---
+++ src/All.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Services from "./components/Services";
+const All = () => {
+  return (
+    <>
+      <Header/>
+      <div className="max-w-7xl mx-auto">
+        <Home/>
+        <About/>
+        <Services/>       
+        <Projects/>
+        <Contact/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default All
